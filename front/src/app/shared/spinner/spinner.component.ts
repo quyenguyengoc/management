1624a49695e66c758c1041cb788a73aa86ee24ef
Quyen Nguyen <quyennn@runsystem.net>
2019-09-09 18:28:43 +0900

    apply template

---
+++ front/src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  Input,
+  OnDestroy,
+  Inject,
+  ViewEncapsulation
+} from '@angular/core';
+
+import {
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+
+export class SpinnerComponent implements OnDestroy {
+  public isSpinnerVisible = true;
+
+  @Input() public backgroundColor = 'rgba(0, 115, 170, 0.69)';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(
+      event => {
+        if (event instanceof NavigationStart) {
+          this.isSpinnerVisible = true;
+        } else if (
+          event instanceof NavigationEnd ||
+          event instanceof NavigationCancel ||
+          event instanceof NavigationError
+        ) {
+          this.isSpinnerVisible = false;
+        }
+      },
+      () => {
+        this.isSpinnerVisible = false;
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.isSpinnerVisible = false;
+  }
+
+}
